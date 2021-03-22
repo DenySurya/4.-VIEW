9f819befbe21a64ae7d1cd299e71f885282ef973
Deny Surya <sgunawan640@gmail.com>
2021-03-22 23:14:35 +0800

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layout/main')
+
+@section('title','Statistik')
+
+
+@section('container')
+
+<!-- body -->
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-3">I Gede Made Deny Surya Gunawan</h1>
+            <h6>1915101037</h6>
+        </div>
+    </div>
+</div>
+
+@endsection
